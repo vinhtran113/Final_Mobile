b86eaf1dfa7722e34b33d62c16b0fb488f1e4176
Triehuynh123 <huynh3896@gmail.com>
2024-05-19 15:58:37 +0700

    api

--- AppBanSach/android/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass ="1234567890";
-$database = "dataonline";
-
-$conn = mysqli_connect($host, $user, $pass, $database);
-mysqli_set_charset($conn, "utf8");
-?>
