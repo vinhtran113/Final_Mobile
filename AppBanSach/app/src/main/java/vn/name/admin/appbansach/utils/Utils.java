3cb6c3d5ad29e3dda5235600f5c160d1159cc97d
Triehuynh123 <huynh3896@gmail.com>
2024-05-19 17:33:08 +0700

    api

--- AppBanSach/app/src/main/java/vn/name/admin/appbansach/utils/Utils.java
+++ AppBanSach/app/src/main/java/vn/name/admin/appbansach/utils/Utils.java
@@ -7,11 +7,9 @@ import vn.name.admin.appbansach.model.GioHang;
 import vn.name.admin.appbansach.model.User;
 
 public class Utils {
-<<<<<<< HEAD
+
     public static final String BASE_URL="http://172.172.31.80/android/";
-=======
-    public static final String BASE_URL="http://10.0.235.88/android/";
->>>>>>> 2ae4aab1da4d121af28cf99fc427d58e5a9d2f5c
+
     public static List<GioHang> manggiohang;
     public static List<GioHang> mangmuahang = new ArrayList<>();
     public static User user_current = new User();
